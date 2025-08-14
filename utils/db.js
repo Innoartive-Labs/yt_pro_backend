a36d150634569bf0933e76e898ba5675aa710352
Dev Adnan <devadnan27@gmail.com>
2025-08-14 22:09:47 +0500

    changed to live

--- utils/db.js
+++ utils/db.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    // host: 'srv444.hstgr.io',
-    // port: 3306,
-    // user: 'u375651769_innoartive',
-    // password: 'Inno2772,,Adnan',
-    // database: 'u375651769_yt_pro'
-    host: '127.0.0.1',
+    host: 'srv444.hstgr.io',
     port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'yt_pro'
+    user: 'u375651769_innoartive',
+    password: 'Inno2772,,Adnan',
+    database: 'u375651769_yt_pro'
+    // host: '127.0.0.1',
+    // port: 3306,
+    // user: 'root',
+    // password: 'root',
+    // database: 'yt_pro'
 });
 
 db.connect((err) => {
